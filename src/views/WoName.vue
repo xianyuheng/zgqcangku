f996a8fcd3578eec1969a1811e1cff81d99cf94a
赵沟渠 <1413834899@qq.com>
2023-05-28 15:04:51 +0800

    添加了按钮功能

--- src/views/WoName.vue
+++ src/views/WoName.vue
@@ -1,5 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const nub = ref(0)
+const hangleclick = () => {
+  nub.value++
+}
+</script>
 <template>
   <div>
     这个是分支页面
+    <button @click="hangleclick">{{nub}}</button>
   </div>
 </template>
