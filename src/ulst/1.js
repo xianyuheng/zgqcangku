92957d107105b2de7936287c77e1b37e812f57f3
赵沟渠 <1413834899@qq.com>
2023-05-28 15:31:17 +0800

    添加了一个公共功能

---
+++ src/ulst/1.js
@@ -0,0 +1,1 @@
+export const LS = console.log('nihao')
--- src/views/WoName.vue
+++ src/views/WoName.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { LS } from '../ulst/1'
 
 const nub = ref(0)
 const hangleclick = () => {
   nub.value++
 }
+onMounted(() => {
+  LS
+})
 </script>
 <template>
   <div>
     这个是分支页面
     <button @click="hangleclick">{{nub}}</button>
   </div>
-</template>
